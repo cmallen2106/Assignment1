0b078d551261049d178d88e5d91691ea1b2c7509
cmallen2106 <cmallen2106@gmail.com>
2021-07-15 21:59:27 -0600

    import scanner and random, create int 1-5

--- Cipher/src/cipher.java
+++ Cipher/src/cipher.java
@@ -2,11 +2,26 @@
 //Christina Sosa
 //Programming Fundamentals, Summer 2021
 
+import java.util.Scanner; 
+import java.util.Random; 
+
 public class cipher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random generator = new Random(); 
+		Scanner scan = new Scanner(System.in); 
+		
+		int key = generator.nextInt(10); 
+		
+		int num1; 
+		int num2; 
+		int num3; 
+		int num4; 
+		int num5; 
+		
+		System.out.println ("Please enter 5 numbers in the range of 0 to 19 down below:"); 
+		
 	}
 
 }
