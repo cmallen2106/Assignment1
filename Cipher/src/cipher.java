67f937eee4bbf473850624dd69fc01ddcd568aa6
cmallen2106 <cmallen2106@gmail.com>
2021-07-15 22:11:00 -0600

    added first number

--- Cipher/src/cipher.java
+++ Cipher/src/cipher.java
@@ -22,6 +22,16 @@ public class cipher {
 		
 		System.out.println ("Please enter 5 numbers in the range of 0 to 19 down below:"); 
 		
+		//1st Number
+		System.out.println("1st Number: ");
+		num1 = scan.nextInt(); 
+		
+		//If 1st number is outside of limit
+		if(0 > num1 | num1 > 19) {
+			System.out.print("Please read directions and try again!"); 
+		}
+		
+		
 	}
 
 }
